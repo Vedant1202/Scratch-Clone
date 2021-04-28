6789b9cdf4f2e1294709fe3ea31e174512a8c5ab
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-04-28 16:54:12 +0530

    Bug fix

--- src/components/Icon/Icon.component.js
+++ src/components/Icon/Icon.component.js
@@ -18,7 +18,7 @@ class Icon extends Component {
    * Render the component
    */
   render() {
-    const { name, size = 20, className = '' } = this.state;
+    const { name, size = 20, className = '' } = this.props;
     return (
       <svg className={`fill-current ${className}`} width={size.toString() + 'px'} height={size.toString() + 'px'}>
         <use xlinkHref={`/icons/solid.svg#${name}`} />
